48c0e3ded70858002868dee8893d89f34e263e26
Rajasekhar Reddy <brsreddy.mca@gmail.com>
2018-12-04 08:48:13 +0530

    added delete and update employee

--- Jpa-curd_example/src/com/blogspot/javabyrajasekhar/Controller/App.java
+++ Jpa-curd_example/src/com/blogspot/javabyrajasekhar/Controller/App.java
@@ -22,7 +22,7 @@ public class App {
 
 		// persisting employee object
 		entityManager.getTransaction().begin();
-		 entityManager.persist(employee);
+		// entityManager.persist(employee);
 		entityManager.getTransaction().commit();
 
 		// get employee by id
@@ -37,8 +37,29 @@ public class App {
 			System.out.println(e3);
 		}
 
-	
-		
+		entityManager.getTransaction().begin();
+
+		Employee e4 = entityManager.find(Employee.class, 1);
+		if (e4 != null) {
+			//entityManager.remove(e4);
+		} else {
+			System.out.println("employee not avaliable");
+		}
+		entityManager.getTransaction().commit();
+
+		// updating employee record
+
+		Employee updateemployee = entityManager.find(Employee.class, 101);
+		entityManager.getTransaction().begin();
+
+		if (updateemployee != null) {
+			updateemployee.setName("rajasekhar");
+			entityManager.persist(updateemployee);
+		} else {
+			System.out.println("employee is not avalibale");
+		}
+
+		entityManager.getTransaction().commit();
 
 		entityManager.close();
 		entityManagerFactory.close();
